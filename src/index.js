c1148d86d369c0b3c6772548e096e543b6f2d273
NearImba <747964230@qq.com>
2018-08-29 23:30:07 +0800

    add unit test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import shapeInput from './shapeInput'
-export default function convertIpInto32int (ip) {
+function convertIpInto32int (ip) {
   ip = shapeInput(ip)
   if (ip) {
     let r = 0
     ip.split('.').forEach((part, index) => {
+      if(index > 3) {
+        return
+      }
       r = r | ((part | 0) << (3 - index) * 8)
     })
     return r >>> 0 // 强制转成无符号整型
   } else {
     console.warn('please provide valid input')
   }
+}
+
+export default {
+  convertIpInto32int,
 }
--- test/index.js
+++ test/index.js
@@ -1,8 +1,10 @@
 const assert = require('assert')
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1)
+const convertIpInto32int = require('../lib/index').convertIpInto32int
+
+describe('iptoint', function () {
+  describe('conver result check', function () {
+    it('should return 2896692481 when input 172.168.5.1', function () {
+      assert.equal(convertIpInto32int('172.168.5.1'), 2896692481)
     })
   })
 })
